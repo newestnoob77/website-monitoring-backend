b26c0e7b01995bce2fb2d5d4c3c4ee1fbee078bc
newestnoob77 <gsjithin1234@gmail.com>
2026-05-05 20:47:26 +0000

    finished monitoring and alert Service

--- backend/src/feature/log/logRepository.js
+++ backend/src/feature/log/logRepository.js
@@ -1,5 +1,5 @@
 import { LogModel } from "./logModel.js";
-
+import mongoose from "mongoose"
 export default class LogRepository {
   async addLog(logDetails) {
     return await new LogModel(logDetails).save();
@@ -9,17 +9,18 @@ export default class LogRepository {
   }
   async getUpTimeReport(websiteId) {
     const result = await LogModel.aggregate([
-      { $match: { websiteId } },
+      { $match: { websiteId: new mongoose.Types.ObjectId(websiteId) } },
       {
         $group: {
           _id: null,
-          total: { sum: 1 },
-          up: { $sum: { $cond: [{ $eq: ["statusCode", 200] }, 1, 0] } },
+          total: { $sum: 1 },
+          up: { $sum: { $cond: [{ $eq: ["$statusCode", 200] }, 1, 0] } },
         },
       },
     ]);
-    if (!result) return { uptime: 0 };
+    if (!result || result.length===0) return { uptime: 0,total:0,up:0 };
+
     const { total, up } = result[0];
-    return { uptime: (up / total) * 100 };
+    return { uptime: (up / total) * 100,total,up };
   }
 }
--- backend/src/feature/monitor/monitor.service.js
+++ backend/src/feature/monitor/monitor.service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import cron from "node ";
+import cron from "node-cron";
 import AlertServices from "./alert.service.js";
 import WebsiteRepository from "../website/website.repository.js";
 import LogRepository from "../log/logRepository.js";
@@ -13,7 +13,7 @@ export default class MonitoringService {
   async checkWebsite(site) {
     try {
       const start = Date.now();
-      const response = await axios(site.url);
+      const response = await axios.get(site.url);
       const responseTime = Date.now() - start;
       await this.logRepository.addLog({
         websiteId: site._id,
@@ -42,7 +42,7 @@ export default class MonitoringService {
       const website = await this.websiteRepository.getAllWebsite();
       for (const site of website) {
         const interval = site.checkInterval || 5;
-        const cronExpression = "";
+        const cronExpression = `*/${interval} * * * *`;
         if (this.jobs[site._id]){
           this.jobs[site._id].stop()
         }
